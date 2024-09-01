c3354324a72968fc2309c756b683d991f55bb7e2
GeorgeYulpatov <toster10703@gmail.com>
2024-09-01 17:11:32 +0300

    Обновлен файл views.py: Docstring

--- diskviewer/views.py
+++ diskviewer/views.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Any
 from urllib.parse import quote
 from django.core.cache import cache
 from django.shortcuts import render
@@ -8,16 +9,25 @@ import requests
 from io import BytesIO
 import zipfile
 
+# Настройка логирования
 logger = logging.getLogger(__name__)
 
+# Базовый URL API Яндекс Диска
 YANDEX_DISK_API_BASE_URL = "https://cloud-api.yandex.net/v1/disk/public/resources"
 
 
-def get_file_list(public_key: str):
+def get_file_list(public_key: str) -> Any | None:
+    """
+    Получает список файлов по публичному ключу с кэшированием.
+
+    :param public_key: Публичный ключ для доступа к файлам.
+    :return: Список файлов или None, если не удалось получить файлы.
+    """
     cache_key = f"file_list_{public_key}"
     cached_files = cache.get(cache_key)
 
-    logger.debug(f"Cached files for {public_key}: {cached_files}")
+    # Логирование кэшированных данных
+    logger.debug(f"Кэшированные файлы для {public_key}: {cached_files}")
 
     if cached_files is not None:
         return cached_files
@@ -26,13 +36,21 @@ def get_file_list(public_key: str):
     response = requests.get(YANDEX_DISK_API_BASE_URL, params=params)
     if response.status_code == 200:
         files = response.json().get('_embedded', {}).get('items', [])
-        logger.debug(f"Fetched files from Yandex Disk: {files}")
-        cache.set(cache_key, files, timeout=600)
+        # Логирование полученных файлов
+        logger.debug(f"Полученные файлы с Яндекс Диска: {files}")
+        cache.set(cache_key, files, timeout=600)  # Кэшируем файлы на 10 минут
         return files
     return None
 
 
-def download_file(public_key: str, path: str):
+def download_file(public_key: str, path: str) -> BytesIO | None:
+    """
+    Скачивает файл по заданному пути и публичному ключу.
+
+    :param public_key: Публичный ключ для доступа к файлу.
+    :param path: Путь к файлу на Яндекс Диске.
+    :return: Содержимое файла в виде BytesIO или None, если файл не найден.
+    """
     params = {'public_key': public_key, 'path': path}
     response = requests.get(f"{YANDEX_DISK_API_BASE_URL}/download", params=params)
     if response.status_code == 200:
@@ -42,20 +60,37 @@ def download_file(public_key: str, path: str):
     return None
 
 
-def index(request):
+def index(request) -> HttpResponse:
+    """
+    Обрабатывает запросы на главную страницу.
+
+    :param request: HTTP запрос.
+    :return: HTTP ответ с формой и списком файлов.
+    """
     files = []
     if request.method == 'POST':
         form = PublicLinkForm(request.POST)
         if form.is_valid():
             public_key = form.cleaned_data['public_key']
             files = get_file_list(public_key)
-            return render(request, 'diskviewer/index.html', {'form': form, 'files': files, 'public_key': public_key})
+            return render(request, 'diskviewer/index.html', {
+                'form': form,
+                'files': files,
+                'public_key': public_key
+            })
     else:
         form = PublicLinkForm()
     return render(request, 'diskviewer/index.html', {'form': form})
 
 
-def get_file_metadata(public_key: str, path: str):
+def get_file_metadata(public_key: str, path: str) -> dict | None:
+    """
+    Получает метаданные файла по публичному ключу и пути.
+
+    :param public_key: Публичный ключ для доступа к файлу.
+    :param path: Путь к файлу на Яндекс Диске.
+    :return: Метаданные файла в виде словаря или None, если файл не найден.
+    """
     params = {'public_key': public_key, 'path': path}
     response = requests.get(YANDEX_DISK_API_BASE_URL, params=params)
     if response.status_code == 200:
@@ -63,19 +98,27 @@ def get_file_metadata(public_key: str, path: str):
     return None
 
 
-def download(request):
+def download(request) -> HttpResponse:
+    """
+    Обрабатывает запрос на скачивание одного файла.
+
+    :param request: HTTP запрос.
+    :return: HTTP ответ с файлом или ошибкой.
+    """
     public_key = request.GET.get('public_key')
     file_path = request.GET.get('file_path')
 
+    # Получение метаданных файла
     file_metadata = get_file_metadata(public_key, file_path)
     if file_metadata is None:
-        return JsonResponse({'error': 'File not found'}, status=404)
+        return JsonResponse({'error': 'Файл не найден'}, status=404)
 
     file_name = file_metadata.get('name', 'downloaded_file')
 
-    # Ensure the filename is correctly encoded
+    # Кодирование имени файла
     encoded_file_name = quote(file_name)
 
+    # Скачивание содержимого файла
     file_content = download_file(public_key, file_path)
     if file_content:
         response = HttpResponse(file_content.getvalue(),
@@ -83,31 +126,37 @@ def download(request):
         response['Content-Disposition'] = f'attachment; filename="{encoded_file_name}"'
         return response
 
-    return JsonResponse({'error': 'File not found'}, status=404)
+    return JsonResponse({'error': 'Файл не найден'}, status=404)
 
 
-def download_multiple(request):
+def download_multiple(request) -> HttpResponse:
+    """
+    Обрабатывает запрос на скачивание нескольких файлов и их упаковку в zip-архив.
+
+    :param request: HTTP запрос.
+    :return: HTTP ответ с zip-архивом или ошибкой.
+    """
     public_key = request.GET.get('public_key')
-    file_paths = request.GET.getlist('file_paths')  # Get multiple file paths from GET parameters
+    file_paths = request.GET.getlist('file_paths')  # Получаем пути к нескольким файлам из GET параметров
 
     if not file_paths:
-        return JsonResponse({'error': 'No files selected'}, status=400)
+        return JsonResponse({'error': 'Не выбраны файлы'}, status=400)
 
     zip_buffer = BytesIO()
     with zipfile.ZipFile(zip_buffer, 'w') as zip_file:
         for file_path in file_paths:
             file_metadata = get_file_metadata(public_key, file_path)
             if file_metadata is None:
-                return JsonResponse({'error': f'File not found: {file_path}'}, status=404)
+                return JsonResponse({'error': f'Файл не найден: {file_path}'}, status=404)
 
             file_name = file_metadata.get('name', 'downloaded_file')
             file_content = download_file(public_key, file_path)
             if file_content:
                 zip_file.writestr(file_name, file_content.getvalue())
             else:
-                return JsonResponse({'error': f'Could not download file: {file_name}'}, status=404)
+                return JsonResponse({'error': f'Не удалось скачать файл: {file_name}'}, status=404)
 
-    zip_buffer.seek(0)  # Go to the beginning of the BytesIO buffer
+    zip_buffer.seek(0)  # Переход к началу буфера BytesIO
     response = HttpResponse(zip_buffer.getvalue(), content_type='application/zip')
     response['Content-Disposition'] = f'attachment; filename="downloaded_files.zip"'
     return response
